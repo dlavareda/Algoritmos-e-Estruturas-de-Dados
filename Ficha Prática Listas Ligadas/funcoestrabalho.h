03cb3d2f42a36af5a8c6e6b1798ce6426cca93c5
dlavareda <diogolavareda9@hotmail.com>
2019-10-06 18:05:55 +0100

    update

--- Ficha Prática Listas Ligadas/funcoestrabalho.h
+++ Ficha Prática Listas Ligadas/funcoestrabalho.h
@@ -1,46 +1,100 @@
 //Exercicio 14 folha pratica 2
 
-typedef struct TRABALHO{
-int numero; //número do aluno;
-int codTrabalho; //identificador do trabalho;
-float nota; //guarda nota do trabalho [0,20]
-struct TRABALHO *nseg;
- }Trabalho;
+typedef struct TRABALHO
+{
+    int numero;      //número do aluno;
+    int codTrabalho; //identificador do trabalho;
+    float nota;      //guarda nota do trabalho [0,20]
+    struct TRABALHO *nseg;
+} Trabalho;
 
- //a - Contar o total de trabalhos feitos por determinado aluno;
-int contarTrabalhosPorAluno(Trabalho *T, int numeroaluno){
-
-
-
-
+Trabalho *makenode()
+{
+    Trabalho *nv;
+    nv = (Trabalho *)malloc(sizeof(Trabalho));
+    nv->nseg = NULL;
+    return (nv);
 }
 
- //b - Calcular a classificação média de um aluno;
-float classificacaoMediaAluno(Trabalho *T, int numeroaluno){
+Trabalho *insertlast(Trabalho *T, Trabalho *nv)
+{
 
+    Trabalho *aux = T;
+    if (T == NULL)
+    {
+        return nv;
+    }
+    while (aux->nseg != NULL)
+    {
+        aux = aux->nseg;
+    }
+    aux->nseg = nv;
+    return T;
 }
- //c - Imprimir o código do trabalho que mais alunos fizeram;
-void trabalhoMaisRealizado(Trabalho *T){
 
+//a - Contar o total de trabalhos feitos por determinado aluno;
+int contarTrabalhosPorAluno(Trabalho *T, int numeroaluno)
+{
+    int count = 0;
+    while (T->nseg != NULL)
+    {
+        if (T->numero == numeroaluno)
+        {
+            count++;
+        }
+        T = T->nseg;
+    }
+    return count;
 }
- //d - Mostrar uma pauta com as classificações dos alunos a determinado trabalho
+
+//b - Calcular a classificação média de um aluno;
+float classificacaoMediaAluno(Trabalho *T, int numeroaluno)
+{
+    float soma = 0.0, media = 0.0;
+    int count = 0;
+    while (T->nseg != NULL)
+    {
+        if (T->numero == numeroaluno)
+        {
+            count++;
+            soma += T->nota;
+        }
+        T = T->nseg;
+    }
+    media = soma / count;
+    return media;
+}
+//c - Imprimir o código do trabalho que mais alunos fizeram;
+void trabalhoMaisRealizado(Trabalho *T)
+{
+}
+//d - Mostrar uma pauta com as classificações dos alunos a determinado trabalho
 
 //d1 Sem ordenação específica;
-void classificacoes(Trabalho *T){
-
+void classificacoes(Trabalho *T, int codTrabalho)
+{
+    while (T->nseg != NULL)
+    {
+        if (T->codTrabalho == codTrabalho)
+        {
+            printf("Aluno %d Nota %f\n", T->numero, T->nota);
+        }
+        T = T->nseg;
+    }
 }
 
 //d2 Mostrando as classificações dos alunos por ordem decresente;
-void classificacoesOrdenadas (Trabalho *T){
-
+void classificacoesOrdenadas(Trabalho *T)
+{
 }
 
 // e - Contar o número de alunos com classificação média superior a um determinado valor;
 
-int contarMediasSuperiores(Trabalho *T, float media){
-
+int contarMediasSuperiores(Trabalho *T, float media)
+{
 }
 //f - Contar o número de alunos que nunca alcançaram uma nota positiva;
-int contarPositivas(Trabalho *T){
-
+int contarNegativas(Trabalho *T)
+{
+  
 }
--- Ficha Prática Listas Ligadas/trabalho.c
+++ Ficha Prática Listas Ligadas/trabalho.c
@@ -2,6 +2,59 @@
 #include <stdlib.h>
 #include "funcoestrabalho.h"
 
-int main(){
+/*int main(){
+    //preencher com dados dummy
+
+    typedef struct TRABALHO{
+int numero; //número do aluno;
+int codTrabalho; //identificador do trabalho;
+float nota; //guarda nota do trabalho [0,20]
+struct TRABALHO *nseg;
+ }Trabalho;*/
+int main()
+{
+    Trabalho *nv, *T = NULL, *aux;
+    int numTrabalhosAluno, numaluno;
+    //preenche a Lista com dados random 1 por aluno
+    for (int i = 0; i < 10; i++)
+    {
+        nv = makenode();
+        nv->codTrabalho = 1;
+        nv->numero = i + 38950;
+        nv->nota = (float)rand() / (float)(RAND_MAX / 20);
+        T = insertlast(T, nv);
+    }
+    //preenche a Lista com dados random 1 por aluno
+    for (int i = 0; i < 10; i++)
+    {
+        nv = makenode();
+        nv->codTrabalho = 2;
+        nv->numero = i + 38950;
+        nv->nota = (float)rand() / (float)(RAND_MAX / 20);
+        T = insertlast(T, nv);
+    }
+    // T = T->nseg;
+    aux = T;
+    while (T != NULL)
+    {
+        printf("Numero %d\n", T->numero);
+        printf("Cod Trabalho %d\n", T->codTrabalho);
+        printf("Nota %f\n", T->nota);
+        printf("\n");
+        T = T->nseg;
+    }
+    T = aux;
+
+    printf("Introduza o numero de aluno \n");
+    scanf("%d", &numaluno);
+    numTrabalhosAluno = contarTrabalhosPorAluno(T, numaluno);
+    printf("O aluno %d realizou %d trabalhos\n", numaluno, numTrabalhosAluno);
+    float mediaAluno;
+    mediaAluno = classificacaoMediaAluno(T, numaluno);
+
+    printf("A classificação média do aluno %d é de %f\n", numaluno, mediaAluno);
+    printf("\nPauta de classificação Trabalho 1\n");
+    classificacoes(T, 1);
     
+
 }
