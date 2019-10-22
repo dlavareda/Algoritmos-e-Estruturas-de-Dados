71b79d6ec2511f79403720bf1b252ec712eec1c4
dlavareda <diogolavareda9@hotmail.com>
2019-10-22 12:46:08 +0100

    update

--- Aula prática 2/PIlhas e FIlas/1.c
+++ Aula prática 2/PIlhas e FIlas/1.c
@@ -12,26 +12,83 @@ typedef struct NODO
 
 int LSBemFormada(Nodo *L)
 {
-    Nodo *aux = L;
-    Nodo *aux2 = L;
-    int nanterior = 0;
-    int nactual = 0;
-    while (aux != NULL)
+    int i;
+    while (L != NULL) // Percorre a lista
     {
-        for (int i = L->salto; i >=0 ; i++)
+        for (i = 0; i < L->salto - 1; i++) //teta para todos os niveis
         {
-            L = L->nseg[i];
-            nactual = L->ID - L->nant[i]->ID;
-            if (nactual < nanterior)
+            if ((L->nseg[i] == NULL) && (L->nseg[i + 1] != NULL)) // Verifica se ha ulma ligação acima que nao ha abaixo
+                return 0;
+            if ((L->nseg[i] != NULL) && (L->nseg[i + 1] != NULL)) // Se ha um salto maior em baixo do que em cima
+            {
+                if (L->nseg[i]->ID > L->nseg[i + 1]->ID)
+                {
+                    return 0;
+                }
+            }
+        }
+        L = L->nseg[0];
+    }
+    return 1;
+}
+
+Nodo *muliplexer(Nodo **A, Nodo **B)
+{
+    Nodo *out;
+    while (*A != NULL || *B != NULL)
+    {
+        if (*A != NULL)
+            out = push(out, pop(A));
+        if (*B != NULL)
+            out = push(out, pop(B));
+    }
+    return out;
+}
+
+void demultiplexer(Nodo **in, Nodo **out_p, Nodo **out_i)
+{
+
+    while (*in != NULL)
+    {
+        *out_p = push(*out_p, pop(in));
+        if (*in != NULL)
+        {
+            *out_i = push(*out_i, pop(in));
+        }
+    }
+}
+
+int ListaCircular(Nodo *L)
+{
+
+    while (L != NULL)
+    {
+        if (L->nseg != NULL)
+        {
+            if (L->nseg->ID < L->ID)
             {
                 return 0;
             }
-            
-            nanterior = nactual;
         }
-        nactual = 0;
-        nanterior = 0;  
+        L = L->nseg;
     }
     return 1;
+}
 
+int ListaCircular(Nodo *L)
+{
+    Nodo *aux;
+    Nodo *act = L;
+    while (act != NULL)
+    {
+        aux = L;
+        while (aux != act)
+        {
+            if (act->nseg == aux)
+                return 1;
+            aux = aux->nseg;
+        }
+        act = act->nseg;
+    }
+    return 0;
 }
