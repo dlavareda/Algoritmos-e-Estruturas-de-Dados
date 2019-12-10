92913c19df15f7746565e3d5e02eab89eb91930e
dlavareda <diogolavareda9@hotmail.com>
2019-12-10 23:17:42 +0000

    update

---
+++ Grafos/labirinto/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int limpar_enter(){
+    while (1)
+    {
+        printf("puta");
+    }
+    
+}
+
+void main(){
+    limpar_enter();
+}
--- Grafos/labirinto/main.c
+++ Grafos/labirinto/main.c
@@ -8,6 +8,31 @@ typedef struct NODO
     struct NODO *nseg;
 } Nodo;
 
+Nodo *makenode()
+{
+    Nodo *nv;
+    nv = (Nodo *)malloc(sizeof(Nodo));
+    nv->custo = 0;
+    nv->id = 0;
+    nv->nseg = NULL;
+    return (nv);
+}
+
+Nodo *insertlast(Nodo *L, Nodo *nv)
+{
+
+    Nodo *aux = L;
+    if (L == NULL)
+    {
+        return nv;
+    }
+    while (aux->nseg != NULL)
+    {
+        aux = aux->nseg;
+    }
+    aux->nseg = nv;
+    return L;
+}
 int **get_tabuleiro(int n)
 {
     int **t = (int **)malloc(n * sizeof(int *));
@@ -45,7 +70,7 @@ Nodo **constroi_grafo(int **t, int n)
             if (t[r - 1][c] != 1)
             {
                 ind_nv = i - 1;
-                *nv = makenode();
+                nv = makenode();
                 nv->id = ind_nv;
                 nv->custo = 1;
                 g[i] = insertlast(g[i], nv);
@@ -56,7 +81,7 @@ Nodo **constroi_grafo(int **t, int n)
             if (t[r + 1][c] != 1)
             {
                 ind_nv = i + 1;
-                *nv = makenode();
+                nv = makenode();
                 nv->id = ind_nv;
                 nv->custo = 1;
                 g[i] = insertlast(g[i], nv);
@@ -67,7 +92,7 @@ Nodo **constroi_grafo(int **t, int n)
             if (t[r][c + 1] != 1)
             {
                 ind_nv = i + n;
-                *nv = makenode();
+                nv = makenode();
                 nv->id = ind_nv;
                 nv->custo = 1;
                 g[i] = insertlast(g[i], nv);
@@ -78,7 +103,7 @@ Nodo **constroi_grafo(int **t, int n)
             if (t[r][c - 1] != 1)
             {
                 ind_nv = i - n;
-                *nv = makenode();
+                nv = makenode();
                 nv->id = ind_nv;
                 nv->custo = 1;
                 g[i] = insertlast(g[i], nv);
@@ -88,6 +113,61 @@ Nodo **constroi_grafo(int **t, int n)
 }
 Nodo *Dijkstra(Nodo **G, int totV, int vo, int vd)
 {
+    int menor, IM=0, n;
+    Nodo *aux;
+    Nodo *L, *nv;
+    int *C = (int *)malloc(totV * sizeof(int));
+    int *E = (int *)malloc(totV * sizeof(int));
+    int *P = (int *)malloc(totV * sizeof(int));
+    for (int i = 0; i < totV; i++)
+    {
+        C[i] = 2147483647;
+        E[i] = 0;
+        P[i] = -1;
+    }
+    C[vd] = 0;
+    for (int i = 0; i < totV; i++)
+    {
+        menor = 2147483647;
+        for (int j = 0; j < totV; j++)
+        {
+            if (C[j] < menor && E[j] != 1)
+            {
+                menor = C[j];
+                IM = j;
+            }
+            E[IM] = 1;
+            aux = G[IM];
+            while (aux != NULL)
+            {
+                if (C[aux->id] > C[IM] + aux->custo)
+                {
+                    C[aux->id] = C[IM] + aux->custo;
+                    P[aux->id] = IM;
+                }
+                aux = aux->nseg;
+            }
+        }
+    }
+    L = NULL;
+    nv = makenode();
+    nv->id = vo;
+    L = insertlast(L, nv);
+    while (1)
+    {
+        n = P[nv->id];
+        nv = makenode();
+        nv->id = n;
+        L = insertlast(L, nv);
+        if (n == vd)
+        {
+            break;
+        }
+    }
+    free(E);
+    free(P);
+    free(C);
+    return L;
 }
 
 void atravessa_labirinto(Nodo **g, int n, int **t)
@@ -97,11 +177,12 @@ void atravessa_labirinto(Nodo **g, int n, int **t)
     Nodo *lista;
     for (int i = 0; i < n * n; i++)
     {
-        if (t[r][c] == 0)
+        ind_z_row_col(i, &r, &c, n);
+        if (t[r][c] == 2)
         {
             vo = i;
         }
-        if (t[r][c])
+        if (t[r][c] == 3)
         {
             vd = i;
         }
@@ -114,10 +195,24 @@ void atravessa_labirinto(Nodo **g, int n, int **t)
         printf("[%d, %d]\n", r, c);
         aux = aux->nseg;
     }
-    lista = clearLista(lista);
+    // lista = clear_list(lista);
 }
 
-void main()
+int main()
 {
-    int **t = get_tabuleiro(5);
+    int N, **tab;
+    Nodo **Grafo;
+    printf("Qual o tamanho do tabuleiro ?\n");
+    scanf("%d", &N);
+    tab = get_tabuleiro(N);
+    Grafo = constroi_grafo(tab, N);
+    atravessa_labirinto(Grafo, N, tab);
+    for (; N > 0; N--)
+    {
+        free(tab[N - 1]);
+        //clear_list(Grafo[N - 1]);
+    }
+    free(tab);
+    free(Grafo);
+    exit(0);
 }
