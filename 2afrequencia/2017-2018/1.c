35fd5bcccefd43c7f0cc19abf4f6959d2dfce612
dlavareda <diogolavareda9@hotmail.com>
2020-01-04 00:01:52 +0000

    update

--- 2afrequencia/2017-2018/1.c
+++ 2afrequencia/2017-2018/1.c
@@ -73,13 +73,122 @@ void imprimeVerticesMenosNPassos(NodoAB *A, int N)
     }
     if (N >= 0)
     {
-        imprimeVerticesMenosNPassos(A->fd, N-1);
+        imprimeVerticesMenosNPassos(A->fd, N - 1);
         printf("%d\n", A->ID);
-        imprimeVerticesMenosNPassos(A->fe, N-1);
-        
+        imprimeVerticesMenosNPassos(A->fe, N - 1);
     }
 }
 
+/*
+2B
+*/
+int todosIDsPositivos(NodoAB *A)
+{ // de forma eficiente 1 - sim 0 - nao
+    if (A == NULL)
+    {
+        return 1;
+    }
+    if (A->ID < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return todosIDsPositivos(A->fe) * todosIDsPositivos(A->fd);
+    }
+}
+
+/*
+2C
+*/
+int contaFE(NodoAB *A)
+{
+    if (A == NULL)
+    {
+        return 0;
+    }
+    if (A->fe != NULL && A->fd != NULL)
+    {
+        return 1 + contaFE(A->fe) + contaFE(A->fd);
+    }
+    else if (A->fe != NULL)
+    {
+        return 1 + contaFE(A->fe);
+    }
+}
+
+int contaFD(NodoAB *A)
+{
+    if (A == NULL)
+    {
+        return 0;
+    }
+    if (A->fd != NULL && A->fe != NULL)
+    {
+        return 1 + contaFD(A->fd) + contaFD(A->fe);
+    }
+    else if (A->fd != NULL)
+    {
+        return 1 + contaFD(A->fd);
+    }
+}
+
+int maisFilhosAtivos(NodoAB *A)
+{ //se ha mais filhos a esquerda retorna -1, se tem mais a direita devolve 1,  se for igual devolve 0
+    if (contaFD(A) > contaFE(A))
+    {
+        return 1;
+    }
+    else if (contaFE(A) > contaFD(A))
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/*
+2D
+*/
+NodoAB *findNode(NodoAB *A, int ID)
+{
+    if (A == NULL)
+    {
+        return;
+    }
+    if (A->ID == ID)
+    {
+        return A;
+    }
+    else
+    {
+        if (findNode(A->fe, ID) != NULL)
+        {
+            return findNode(A->fe, ID);
+        }
+        if (findNode(A->fd, ID) != NULL)
+        {
+            findNode(A->fd, ID);
+        }
+    }
+}
+
+int arvoresDisjuntas(NodoAB *A, NodoAB *B)
+{ //1 Sim - 0 Não sao disjuntas se nao tiverem NÓS com IDs iguais
+    if (A == NULL)
+    {
+        return 1;
+    }
+    if (findNode(B, A->ID) !=NULL)
+    {
+        return 0;
+    }else{
+        return arvoresDisjuntas(A->fe, B) * arvoresDisjuntas(A->fd, B);
+    }
+    
+}
 int main()
 {
     NodoAB *AB = NULL, *nv = NULL;
@@ -97,7 +206,16 @@ int main()
     AB = treeInsert(AB, *nv);
     nv = makenode(8, "Bragança");
     AB = treeInsert(AB, *nv);
+    
+    NodoAB *AB2 = NULL;
+    nv = makenode(15, "Paris");
+    AB2 = treeInsert(AB2, *nv);
 
     imprimeVerticesMenosNPassos(AB, 1);
+    printf("Todos IDs positivos? %d\n", todosIDsPositivos(AB));
+    printf("Conta FE =  %d\n", contaFE(AB));
+    printf("Conta FD =  %d\n", contaFD(AB));
+    printf("Existem mais FE do que FD? %d\n", maisFilhosAtivos(AB));
+    printf("Arvores disjuntas? %d\n", arvoresDisjuntas(AB, AB2));
     printf("pause");
 }
